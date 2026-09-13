fde3f80c043d294027687afec2ff8590001f4006
lalitmohanbhakuni <lalitmohanbhakuni@gmail.com>
2026-09-13 20:48:50 +0530

    add

---
+++ core/management/commands/create_superuser.py
@@ -0,0 +1,31 @@
+# core/management/commands/create_superuser.py
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+class Command(BaseCommand):
+    help = 'Creates superuser from env vars if it does not exist.'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+        email    = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+        if not all([username, email, password]):
+            self.stdout.write(self.style.WARNING(
+                'Superuser creation skipped: env vars not set.'
+            ))
+            return
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write(self.style.SUCCESS(
+                f'Superuser "{username}" already exists.'
+            ))
+            return
+
+        User.objects.create_superuser(username=username, email=email, password=password)
+        self.stdout.write(self.style.SUCCESS(
+            f'Superuser "{username}" created successfully.'
+        ))
